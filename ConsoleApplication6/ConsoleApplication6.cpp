835c8368b53b5197d9da2cf8cf81b856a74f4efe
dems 1 <galkmat@hotmail.com>
2025-06-14 11:25:41 +0300

    Вставлен новый код. Проанализированн и дополнен комментариями

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -2,71 +2,120 @@
 //
 
 #include <iostream>
+using namespace std;
 
-class playerPoint 
+
+class Player
 {
-
 public:
-    void bubbleSort(int listLength)
-    {
-        int** list = new int* [listLength];
-        for (int i = 0; i < 2; i++)
-        {
-            list[i] = new int[1];
-        }
-        for (int i = 0; i < listLength; i++)
-        {
-            std::cout << "Enter name " << i+1 << " player: ";
-            std::cin >> list[i][0];
-            std::cout << "Enter ponints " << i+1 << " player: ";
-            std::cin >> list[i][1];
-            system("cls");
-        }
-        while (listLength)
-        {
-            bool swapped = false;
+	string GetName()
+	{
+		return name;
+	}
+	Player(string newName, int newScore)
+	{
+		// Прередаем полученную информацию из строки 104 в переменные класса
+		this->name = newName;
+		this->score = newScore;
+	}
+	int GetScore()
+	{
+		return score;
+	}
+	
+	Player();
+	/*
+	~Player();
+	*/
 
-            for (int i = 0; i < listLength - 1; i++)
-            {
-                if (list[i][1] < list[i + 1][1])
-                {
-                    std::swap(list[i + 1], list[i]);
-                    swapped = true;
-                }
-            };
-            if (swapped == false)
-            {
-                system("cls");
-                for (int i = 0; i < listLength; i++)
-                {
-                    for (int j = 0; j < 2; j++)
-                    {
-                        std::cout << list[i][j];
-                    }
-                    std::cout << '\n';
-                }
-                for (int i = 0; i < listLength; i++)
-                {
-                    delete list[0];
-                    list[i] = nullptr;
-                }
+	// Метод при помощи которого происходит сосртировка пользователей
+	void shakersort(Player arr[], int end) {
 
-                break;
-            }
-        }
-    }
+
+		bool b = true;
+		//Player* beg = first-1;
+		//Player* end = first+size;
+		int beg = -1;
+		while (b) {
+			b = false;
+			beg++;
+			// Перебираем массив из пользователей
+			for (int i = beg; i < end; i++) {
+				// Если кол-во очков i-го игрока больше чем i+1 , то мы меняем их местами
+				// 
+				// Была ошибка. Цикл выходил за границы масива и выдавал значение из 
+				// несуществующей ячейки. Исправил путем добавления доп. условия. 
+				if ((arr[i].score < arr[i + 1].score) && (arr[i+1].name != ""))
+				{
+					swap(*(arr + i), *(arr + i + 1));
+					b = true;
+				}
+			}
+			if (!b) break;
+			end--;
+			// Доп. проверка массива на правильность с конца массива
+			for (int i = end; i > beg; i--) {
+				if (arr[i].score > arr[i - 1].score)
+				{
+					swap(*(arr + i), *(arr + i + -1));
+					b = true;
+				}
+			}
+		}
+	}
 private:
-    int nameCharacter, pointCharacter;
+	string name;
+	int score;
 };
 
 
-int main() 
+// Конструктор по упомлчанию
+Player::Player()
 {
-    playerPoint countPlayer;
-    int count = 0;
-    std::cout << "Enter player count ";    
-    std::cin >> count;
+}
 
-    countPlayer.bubbleSort(count);
+/* Не понял для чего этот конструктор
+Player ::~Player()
+{
+}
+*/
+
+
+int main()
+{
+	int quant;
+	// Получение информации от пользователя о кол-ве игроков
+	cout << "Please enter the quantity of players" << endl;
+	cin >> quant;
+
+	// Создаем динамический экземпляр класса 
+	Player* arr = new Player[quant];
+	// Заполняем поочередно массив игроков 
+	for (int i = 0; i < quant; i++)
+	{
+		string newName;
+		int newScore;
+		//Cпрашиваем информацию о игроках у пользователя
+		cout << "Please enter the name of player #" << i + 1 << endl;
+		cin >> newName;
+		cout << "Please enter the score of player #" << i + 1 << endl;
+		cin >> newScore;
+		// Присваиваем информация об игроках в массив
+		arr[i] = Player(newName, newScore);	
+	}
+
+	// Разименовываем указатель arr и передаем информацию в метод shakersort
+	arr->shakersort(arr, quant);
+
+	//Вывод элемментов массива в отсортированном порядке
+	for (int i = 0; i < quant; i++)
+	{
+
+		cout << arr[i].GetName() << '\t' << arr[i].GetScore() << endl;
+
+	}
+
+	// Очистка выделенной памяти под дин. массив
+	delete[] arr;
 }
 
